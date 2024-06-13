560cdfdf944f65e71f52068acaa3918167f65535
DimasBird <dagutnikov@edu.hse.ru>
2024-06-13 19:14:34 +0300

    Remade docs

--- docs/html/search/functions_1.js
+++ docs/html/search/functions_1.js
@@ -4,8 +4,11 @@ var searchData=
   ['changeflashlight_1',['ChangeFlashlight',['../class_a_my_character.html#a2eb5ec204357aa495f29387e070d4910',1,'AMyCharacter::ChangeFlashlight()'],['../class_a_night_guard.html#ae7388b4c115adffc52b37112294c68b9',1,'ANightGuard::ChangeFlashlight()']]],
   ['cleanuppreinitcontext_2',['CleanupPreInitContext',['../class_f_engine_loop.html#ae3548aa102fbbe22cc36a501258161db',1,'FEngineLoop']]],
   ['client_5fsetcurrentstamina_3',['Client_SetCurrentStamina',['../class_a_night_guard.html#a9be2522150e4604ceb24556acae4f856',1,'ANightGuard']]],
-  ['client_5fsetisrun_4',['Client_SetIsRun',['../class_a_night_guard.html#a16905f4c170e279d93e304b23be63002',1,'ANightGuard']]],
-  ['client_5fsetistired_5',['Client_SetIsTired',['../class_a_night_guard.html#a6b2515903a6ae4eca94de79be8a55b8c',1,'ANightGuard']]],
-  ['createsubsystem_6',['CreateSubsystem',['../class_i_online_factory.html#abeb86e85618722fdcf504b35a9974bb5',1,'IOnlineFactory']]],
-  ['crouchfunc_7',['CrouchFunc',['../class_a_my_character.html#afa4d4cdc45c33537fe1ebb640ddf10ea',1,'AMyCharacter::CrouchFunc()'],['../class_a_night_guard.html#abaada86c3663e3d99bff8d56a56c56d9',1,'ANightGuard::CrouchFunc()']]]
+  ['client_5fsetcurrentstamina_5fimplementation_4',['Client_SetCurrentStamina_Implementation',['../class_a_night_guard.html#a8659aa1605d870d6d4071bb598b8ea29',1,'ANightGuard']]],
+  ['client_5fsetisrun_5',['Client_SetIsRun',['../class_a_night_guard.html#a16905f4c170e279d93e304b23be63002',1,'ANightGuard']]],
+  ['client_5fsetisrun_5fimplementation_6',['Client_SetIsRun_Implementation',['../class_a_night_guard.html#af2dc632e9d0ae934b972c04815aec21b',1,'ANightGuard']]],
+  ['client_5fsetistired_7',['Client_SetIsTired',['../class_a_night_guard.html#a6b2515903a6ae4eca94de79be8a55b8c',1,'ANightGuard']]],
+  ['client_5fsetistired_5fimplementation_8',['Client_SetIsTired_Implementation',['../class_a_night_guard.html#a07399ccd4689838eac53076871aef281',1,'ANightGuard']]],
+  ['createsubsystem_9',['CreateSubsystem',['../class_i_online_factory.html#abeb86e85618722fdcf504b35a9974bb5',1,'IOnlineFactory']]],
+  ['crouchfunc_10',['CrouchFunc',['../class_a_my_character.html#afa4d4cdc45c33537fe1ebb640ddf10ea',1,'AMyCharacter::CrouchFunc()'],['../class_a_night_guard.html#abaada86c3663e3d99bff8d56a56c56d9',1,'ANightGuard::CrouchFunc()']]]
 ];
